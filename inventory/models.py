ce9a7a8bc29c71db74bf6cadb755f8301533dd80
Rodrigo da Silva <rigogsilva@gmail.com>
2017-12-23 19:01:00 -0600

    Added integration to model.

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 class Item(models.Model):
-	tittle = models.CharField(max_length=200)
+	title = models.CharField(max_length=200)
 	description = models.TextField()
 	amount = models.IntegerField()
